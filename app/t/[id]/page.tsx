f1dc482f3defe9292c3131ebd5814493738c681f
Joey Baker <joey@joeybaker.co>
2024-06-17 15:05:52 -0700

    Add loading state
    …that was too easy.

--- app/t/[id]/page.tsx
+++ app/t/[id]/page.tsx
@@ -8,29 +8,14 @@ export default function Thought({
 }: {
   params: { id: string }
 }) {
-  // TODO: loading state as a client component
-  const isLoading = false
   const { title, notes } = getPost({ id })
 
   return (
     <article className={styles.box}>
-      {isLoading ? (
-        <h2 className={styles.titleLoading}>
-          &#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#8199;&emsp;
-          &#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;
-        </h2>
-      ) : (
-        <h2 className={styles.title}>{title}</h2>
-      )}
-      {isLoading ? (
-        <div className={styles.bodyLoading}>
-          &#9608;&#9608;&#9608;&#9608;&ensp;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&ensp;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&ensp;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&ensp;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&ensp;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&ensp;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&ensp;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&ensp;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&ensp;&#9608;&#9608;&#9608;&#9608;&#9608;&#9608;&ensp;&#9608;&#9608;
-        </div>
-      ) : (
-        <div className={styles.body}>
-          <Markdown>{notes}</Markdown>
-        </div>
-      )}
+      <h2 className={styles.title}>{title}</h2>
+      <div className={styles.body}>
+        <Markdown>{notes}</Markdown>
+      </div>
     </article>
   )
 }
